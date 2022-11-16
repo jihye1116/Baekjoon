d198b1bad65629649adad2a434ce328b9dda5b42
Wickhye <jihyesim170@gmail.com>
2022-11-16 17:15:07 +0900

    2022.11.17

---
+++ 18108.py
@@ -0,0 +1,4 @@
+year = int(input())
+
+bulgyo = year - 543
+print(bulgyo)
---
+++ 3003.py
@@ -0,0 +1,4 @@
+need = [1, 1, 2, 2, 2, 8]
+chess = list(map(int, input().split()))
+for i in range(6):
+    print(need[i]-chess[i],end=' ')
