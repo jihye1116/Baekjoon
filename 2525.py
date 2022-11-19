dc2e02c03e72a692378d1c4c095bc9c12cefe7be
Wickhye <jihyesim170@gmail.com>
2022-11-19 19:41:48 +0900

    2022.11.19

---
+++ 2525.py
@@ -0,0 +1,13 @@
+hour, min= map(int, input().split())
+cooking = int(input())
+
+hour += int(cooking /60)
+min += cooking % 60
+
+if min >= 60:
+    hour += 1
+    min -= 60
+if hour >= 24:
+    hour -= 24
+
+print(hour, min)
