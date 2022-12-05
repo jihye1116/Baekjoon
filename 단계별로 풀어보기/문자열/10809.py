b0e9a8d533c4944622feea1072902557f3ff43f8
Wickhye <jihyesim170@gmail.com>
2022-12-05 12:10:57 +0900

    2022.12.05

--- 단계별로 풀어보기/문자열/10809.py
+++ 단계별로 풀어보기/문자열/10809.py
@@ -1,12 +1,4 @@
 # 알파벳 찾기
-
 S = input()
- 
-for i in range(ord('a'),ord('z')+1):
-    if(ord(j) == i):
-            print(S.index(j), end=" ")
-    else:
-        print(-1, end=" ")
-
-# a = list(map(int, input().split()))
-# print(len(a))
+for x in 'abcdefghijklmnopqrstuvwxyz':
+    print(S.find(x), end = ' ')
