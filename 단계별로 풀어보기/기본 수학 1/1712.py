bc4179d78f1aa9177e4334ea069cca6ed07e3f61
Wickhye <jihyesim170@gmail.com>
2022-12-30 23:31:26 +0900

    2022.12.30

---
+++ 단계별로 풀어보기/기본 수학 1/1712.py
@@ -0,0 +1,5 @@
+A,B,C = map(int, input().split()) # A: 고정 비용, B: 가변 비용, C: 노트북 가격, n: 노트북 대수
+if B>=C: 
+    print("-1") # 가변 비용이 노트북 가격보다 클 때
+else: 
+    print(A//(C-B)+1) # 판매수입: A+nB=nc  -> n(B+C)=A  -> ∴ n=A/(B+C) 
